aff204ed9b9e37c39eb72cae21bc63479f7f73ff
mintandgirls <sanek228art@gmail.com>
2025-02-23 21:18:20 +0300

    добавил ввод с клавиатуры

--- main.cpp
+++ main.cpp
@@ -3,12 +3,9 @@
 #include <vector>
 #include <limits>
 #include <algorithm>
-
+#include <locale>
 
 using namespace std;
-
-int GetCorrectValue();
-void FixStreamState();
 
 struct date{
     int day;
@@ -26,15 +23,28 @@ struct product{
     string chiefSurname;
 };
 
+product Add();
+int GetCorrectValue();
+void FixStreamState();
+
+//МЕНЮ
+void PrintMenu();
 
 int main() {
+    setlocale(LC_ALL, "Russian");
     int command{};
+    vector<product> products;
     do {
+        PrintMenu();
         command = GetCorrectValue();
         switch (command) {
             case 1:
+                products.push_back(Add());
                 break;
             case 2:
+                for(int i = 0; i < products.size(); i++) {
+                    cout << products[i].company << endl;
+                }
                 break;
             case 3:
                 break;
@@ -45,7 +55,7 @@ int main() {
             case 0:
                 cout << "The program is over" << endl; break;
             default:
-                cout << "РќРµРёР·РІРµСЃС‚РЅР°СЏ РєРѕРјР°РЅРґР°" << endl;
+                cout << "Неизвестная команда" << endl;
         }
     } while (command);
     return 0;
@@ -63,8 +73,8 @@ int GetCorrectValue() {
         isNotOk = false;
         if ((cin >> n).fail()) {
             FixStreamState();
-            cout << "РќРµРёР·РІРµСЃС‚РЅР°СЏ РєРѕРјР°РЅРґР°" << endl;
-            cout << "Р’РІРµРґРёС‚Рµ РєРѕРјР°РЅРґСѓ РµС‰Рµ СЂР°Р·" << endl;
+            cout << "Неизвестная команда" << endl;
+            cout << "Введите команду еще раз" << endl;
             isNotOk = true;
         }
     } while (isNotOk);
@@ -72,11 +82,58 @@ int GetCorrectValue() {
     return n;
 }
 
-// РЎРћР РўРР РћР’РљРђ РџРћ Р”РђРўР•
-bool compareByDate(const product &a, const product &b) {
-    if (a.date.year != b.date.year)
-        return a.date.year < b.date.year;
-    if (a.date.month != b.date.month)
-        return a.date.month < b.date.month;
-    return a.date.day < b.date.day;
+// Ввод данных
+product Add() {
+    product product;
+    
+    cout << "Введите название предприятия: ";
+    cin.ignore();
+    getline(cin, product.company);
+    
+    cout << "Введите название цеха: ";
+    getline(cin, product.workshop);
+    
+    cout << "Введите наименование продукции: ";
+    getline(cin, product.productName);
+    
+    cout << "Введите количество единиц в партии: ";
+    cin >> product.productCount;
+    FixStreamState();
+    
+    cout << "Введите день выпуска продукции: ";
+    cin >> product.date.day;
+    FixStreamState();
+    
+    cout << "Введите месяц выпуска продукции: ";
+    cin >> product.date.month;
+    FixStreamState();
+    
+    cout << "Введите год выпуска продукции: ";
+    cin >> product.date.year;
+    FixStreamState();
+    
+    cout << "Введите район, в котором находится предприятие: ";
+    getline(cin, product.districtOfCompany);
+    
+    cout << "Введите фамилию начальника цеха: ";
+    getline(cin, product.chiefSurname);
+    
+    return product;
 }
+
+// Сортировка по дате
+// bool compareByDate(const product &a, const product &b) {
+//     if (a.date.year != b.date.year)
+//         return a.date.year < b.date.year;
+//     if (a.date.month != b.date.month)
+//         return a.date.month < b.date.month;
+//     return a.date.day < b.date.day;
+// }
+
+// Основное меню
+void PrintMenu() {
+    cout << "1 - Добавить продукцию" << endl;
+    cout << "2 - Показать таблицу" << endl;
+
+}
+//
