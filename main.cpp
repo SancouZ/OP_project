36c92d635669d6a5716ba86785f3e58fb96193dc
mintandgirls <sanek228art@gmail.com>
2025-02-23 11:37:43 +0300

    3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <limits>
+
+
+using namespace std;
+
+int GetCorrectValue();
+void FixStreamState();
 
 
 int main() {
     
+}
+
+void FixStreamState() {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
+int GetCorrectValue() {
+    int n{};
+    bool isNotOk{};
+    do {
+        isNotOk = false;
+        if ((cin >> n).fail()) {
+            FixStreamState();
+            cout << "Неизвестная команда" << endl;
+            cout << "Введите команду еще раз" << endl;
+            isNotOk = true;
+        }
+    } while (isNotOk);
+
+    return n;
 }
