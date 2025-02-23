36a1f2b14de95c96e12a2abe505981dec9a2cacc
mintandgirls <sanek228art@gmail.com>
2025-02-23 12:26:20 +0300

    создал структуры

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <limits>
+#include <algorithm>
 
 
 using namespace std;
@@ -9,9 +10,45 @@ using namespace std;
 int GetCorrectValue();
 void FixStreamState();
 
+struct date{
+    int day;
+    int month;
+    int year;
+};
+
+struct product{
+    string company;
+    string workshop;
+    string productName;
+    int productCount;
+    date date;
+    string districtOfCompany;
+    string chiefSurname;
+};
+
 
 int main() {
-    
+    int command{};
+    do {
+        command = GetCorrectValue();
+        switch (command) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 0:
+                cout << "The program is over" << endl; break;
+            default:
+                cout << "Неизвестная команда" << endl;
+        }
+    } while (command);
+    return 0;
 }
 
 void FixStreamState() {
@@ -33,4 +70,13 @@ int GetCorrectValue() {
     } while (isNotOk);
 
     return n;
+}
+
+// СОРТИРОВКА ПО ДАТЕ
+bool compareByDate(const product &a, const product &b) {
+    if (a.date.year != b.date.year)
+        return a.date.year < b.date.year;
+    if (a.date.month != b.date.month)
+        return a.date.month < b.date.month;
+    return a.date.day < b.date.day;
 }
